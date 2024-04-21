c0c1fb5e6a757ed3b79985bb1433e61b76646d39
Cristina Carrasco <cristynakity@gmail.com>
2024-04-21 01:01:16 -0500

    Feature/Add more content and description to search component. (#1)
    * Added new files from new sync
    
    * Add more missing Htmls
    
    * Add more missing files
    
    * Change search and update search.json
    
    ---------
    
    Co-authored-by: Cristina Carrasco <cristinakity@hotmail.com>

--- CreateSearchJsonList.ps1
+++ CreateSearchJsonList.ps1
@@ -22,12 +22,32 @@ foreach ($file in $htmlFiles) {
 
     # Get the HTML meta data keywords (assuming they are stored in a <meta name="keywords" content="..." /> tag)
     $metaKeywordsMatch = (Get-Content -Path $file.FullName | Select-String -Pattern '<meta name="keywords" content="([^"]+)"' -AllMatches).Matches
-    if ($metaKeywordsMatch) {
+    # Get the HTML meta data description (assuming it is stored in a <meta name="description" content="..." /> tag)
+    $metaDescriptionMatch = (Get-Content -Path $file.FullName | Select-String -Pattern '<meta name="description" content="([^"]+)"' -AllMatches).Matches
+
+    $metaKeywords = if ($metaKeywordsMatch) { $metaKeywordsMatch.Groups[1].Value } else { "" }
+    # create a new string with the description, remove the commas and common words
+    $description = if ($metaDescriptionMatch) { $metaDescriptionMatch.Groups[1].Value } else { "" }
+    # Add description to the meta keywords
+    # validate if the description is not empty
+    if ($description) {
+        $description = $description -replace ',', ''
+        # $commonWords = "common_word1", "common_word2", "common_word3", "the", "and", "in", "on"
+        # $description = $description -replace ($commonWords -join "|"), ''
+
+        # Add the description to the meta keywords
+        # validate if metaKeywords is not empty
+        if ($metaKeywords) {
+            $metaKeywords += ", " + $description
+        } else {
+            $metaKeywords = $description
+        }
+    }
+
+    if ($metaKeywords) {
         $headContent = Get-Content -Path $file.FullName | Out-String
         $pageTitle = [regex]::Match($headContent, '<title>(.*?)<\/title>').Groups[1].Value
      
-        $metaKeywords = $metaKeywordsMatch.Groups[1].Value
-
         $urlPath = $file.FullName -replace [regex]::Escape($partToRemove), ""
 
         # Create an object with the URL path and keywords
@@ -35,6 +55,7 @@ foreach ($file in $htmlFiles) {
             UrlPath = "." + $urlPath
             Title = $pageTitle
             Keywords = $metaKeywords -split ','
+            Description = $metaDescriptionMatch.Groups[1].Value
         }
 
         # Add the result to the array
@@ -50,16 +71,30 @@ foreach ($file in $htmlFiles) {
 
     # Show progress
     $percentage = [Math]::Round(($results.Count / $htmlFiles.Count) * 100, 2)
-    Write-Progress -Activity "Generating Search json" -Status "Processed file: $($fileName)" -PercentComplete $percentage
+    $percentage = if ($percentage -gt 100) { 100 } else { $percentage }
+    Write-Progress -Activity "Generating Search json" -Status "Processed file $($percentage)% $($fileName)" -PercentComplete $percentage
     # Write-Progress "Processed file: $($fileName)" -PercentComplete $percentage
     # Calculate the percentage of total HTML files processed and round it to 2 decimal places
     # Write-Progress "Percentage of total HTML files processed: $($percentage)%"
+    if($percentage -eq 100) {
+        Write-Progress -Activity "Generating Search json" -Status "Completed" -Completed
+        break;
+    }
 }
+# Read the whole file as string
+$fileContent = Get-Content -Path $resultJsonFilePath
+# Remove the last comma from the JSON file
+$lineCount = $fileContent.Count
+$index = 0;
+$fileContent = foreach ($line in $fileContent) {
+    $index++
+    if ($lineCount -eq $index) {
+        $line -replace ',\s*$', ''
+    } else {
+        $line
+    }
+}
+# Save the updated content back to the file
+$fileContent | Set-Content -Path $resultJsonFilePath
+# Add the closing square bracket to the JSON file
 ']' | Out-File -FilePath $resultJsonFilePath -Append
-
-# # Convert the results array to JSON format
-# $resultsJson = $results | ConvertTo-Json
-
-# # Save the JSON data to a file
-# $searchJsonFilePath = "$srcFolderPath/search.json"
-# $resultsJson | Out-File -FilePath $searchJsonFilePath
--- forge.config.js
+++ forge.config.js
@@ -41,4 +41,17 @@ module.exports = {
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'cristinakity',
+          name: 'offline-stardew-valley-wiki'
+        },
+        prerelease: false,
+        draft: true
+      }
+    }
+  ]
 };
